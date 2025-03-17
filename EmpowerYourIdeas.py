b8af71460a6f0188721cc1b7e59805cd563c31c3
Brian Trinh <145734454+BrianTrinh25@users.noreply.github.com>
2025-03-17 14:45:15 -0700

    Update EmpowerYourIdeas.py

--- EmpowerYourIdeas.py
+++ EmpowerYourIdeas.py
@@ -3,12 +3,13 @@ import openai
 import streamlit as st 
 import time
 # from IPython.display import Image, display
+from openai import OpenAI
 import pandas as pd
 import toml
 
 #my-api-key-here
 openai.api_key = os.environ["OPENAI_API_KEY"]
-client = openai() 
+client = OpenAI() 
 
 #st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 #st.sidebar.image("Logo.jpg", use_column_width=True)
@@ -75,3 +76,6 @@ with st.form(key = "chat"):
                    facing the City of San Jose, you can make a real impact on global issues and contribute to \
                    a more sustainable future. If you would like to explore more project ideas, feel free to \
                    rerun this application. Thank you for using Empower Your Ideas, and best of luck with your projects!")
+
+
+
